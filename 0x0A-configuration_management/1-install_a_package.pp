c8f71362ac8654e136baa80da5582b2be823dee9
BirtukanK <birtukankuma1113@gmail.com>
2024-01-19 10:24:20 -0800

    install package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,13 @@
+# Defines a Puppet manifest to install Flask using pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Exec['update_pip3'],
+}
+
+# Update pip3 to the latest version
+exec { 'update_pip3':
+  command => 'pip3 install --upgrade pip',
+  path    => '/usr/bin',
+  unless  => 'pip3 show pip | grep -q "Version: 21."',
+}
