ede62646a71e4b79d072feffc2dcb151671dcce8
BirtukanK <birtukankuma1113@gmail.com>
2024-02-19 04:18:05 -0800

    REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,25 +5,22 @@ import requests
 import sys
 """ all necessary modules imported"""
 
-url = "https://jsonplaceholder.typicode.com"
-employee_id = int(sys.argv[1])
-
-user_response = requests.get(f"{url}/users/{employee_id}")
-todos_response = requests.get(f"{url}/todos?userId={employee_id}")
-
-user_data = user_response.json()
-todos_data = todos_response.json()
-employee_name = user_data.get("name")
-
-completed_tasks = [task for task in todos_data if task["completed"]]
-number_of_done_tasks = len(completed_tasks)
-total_number_of_tasks = len(todos_data)
-
-print(f"Employee {employee_name} is done with tasks\
-      ({number_of_done_tasks}/{total_number_of_tasks}): ")
-for task in completed_tasks:
-    print(f"\t{task['title']}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        sys.exit(1)
+    url = "https://jsonplaceholder.typicode.com"
+    employee_id = int(sys.argv[1])
+
+    user_response = requests.get(f"{url}/users/{employee_id}")
+    todos_response = requests.get(f"{url}/todos?userId={employee_id}")
+
+    user_data = user_response.json()
+    todos_data = todos_response.json()
+    employee_name = user_data.get("name")
+
+    completed_tasks = [task for task in todos_data if task["completed"]]
+    number_of_done_tasks = len(completed_tasks)
+    total_number_of_tasks = len(todos_data)
+
+    print(f"Employee {employee_name} is done with tasks\
+          ({number_of_done_tasks}/{total_number_of_tasks}): ")
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
