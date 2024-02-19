766415e5071b04f49b1c1626e29602d1f3b2e633
BirtukanK <birtukankuma1113@gmail.com>
2024-02-19 04:14:47 -0800

    REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -19,7 +19,11 @@ completed_tasks = [task for task in todos_data if task["completed"]]
 number_of_done_tasks = len(completed_tasks)
 total_number_of_tasks = len(todos_data)
 
-print(f"Employee {employee_name} is done with tasks
+print(f"Employee {employee_name} is done with tasks\
       ({number_of_done_tasks}/{total_number_of_tasks}): ")
 for task in completed_tasks:
     print(f"\t{task['title']}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
