44280612a6057a16f01a651a6662a9dd7b809ca7
BirtukanK <birtukankuma1113@gmail.com>
2024-02-19 04:35:58 -0800

    REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Defines a python script to acess REST API
 """
-import requests
 import sys
+import requests
 """ all necessary modules imported"""
 
 if __name__ == "__main__":
