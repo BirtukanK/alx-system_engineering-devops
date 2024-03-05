24ed6267bdc8b70f2b090b27551f91000b31deb2
BirtukanK <birtukankuma1113@gmail.com>
2024-03-05 11:35:38 +0000

    number of subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines method of subscribers"""
+
+
+def number_of_subscribers(subreddit):
+    """ Queryis api an return number of subscribers"""
+    import requests
+
+    info = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit),
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
+    if info.status_code >= 300:
+        return 0
+
+    return info.json().get("data").get("subscribers")
